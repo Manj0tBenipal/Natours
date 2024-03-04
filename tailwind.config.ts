33126e49e51363dc85415f3d738b7870eaccbf7e
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-04 14:05:20 -0500

    Added nextui configuration  for button and navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,16 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input|navbar).js",
   ],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
