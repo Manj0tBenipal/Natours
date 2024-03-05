a8d8cd52c382e9d5a9320bd9ab0e95e46647df28
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-05 11:01:40 -0500

    typo fixesa nd used custom class for gradient text to maintain consistency across pages

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,16 +5,14 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="max-w-full grid grid-cols-2 h-dvh">
-      <div className="flex flex-col space-y-4	font-extrabold mt-32 p-10">
+      <div className="flex flex-col space-y-4	font-extrabold mt-32 ">
         <p className="text-4xl	font-extrabold">
           Tours that bring once in a
-          <span className="text-transparent bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text">
-            &nbsp; lifetime &nbsp;
-          </span>
+          <span className="text-gradient">&nbsp; lifetime &nbsp;</span>
           moment. Explore What we offer.
         </p>
         <p className="font-medium w-5/6 ">
-          Natous is Canada based company that offers tour services acroos the
+          Natours is Canada based company that offers tour services acroos the
           world. With each tour featuring a local guide, You&apos;ll explore
           every wonder that a destination has to offer!
         </p>
