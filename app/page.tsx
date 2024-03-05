99a8b7dc0bedabcc771ac6bb4e40f6eb22a8dbf0
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-05 11:00:13 -0500

    Added content section to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ContentSection from "@/components/ContentSection";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="w-full">
+    <main className="w-full p-16">
       <Hero />
+      <ContentSection />
     </main>
   );
 }
