be776d41c40e26621f02b1abcb286d8fb9e83cf5
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-04 23:28:42 -0500

    added hero component to root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="">
-
+    <main className="w-full">
+      <Hero />
     </main>
   );
 }
