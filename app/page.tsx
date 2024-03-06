d44bdf8dede6cf31c172727ccac61049537d5097
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-06 14:14:19 -0500

    removed main tag from Home page and moved it to layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="w-full p-16">
+    <>
       <Hero />
       <ContentSection />
       <Footer />
-    </main>
+    </>
   );
 }
