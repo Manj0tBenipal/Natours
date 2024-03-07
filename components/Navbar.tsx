32a64b5021f331d57daec8ae11877408b6ae5776
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-07 08:22:19 -0500

    added logon form to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,32 +7,46 @@ import {
   NavbarItem,
   Button,
 } from "@nextui-org/react";
+import LoginForm from "./LoginForm";
+import { useState } from "react";
 export default function NavbarComponent() {
+  const [loginFormVisible, setLoginFormVisible] = useState<boolean>(false);
   return (
-    <Navbar className="flex min-w-full  shadow-sm">
-      <NavbarContent justify="start">
-        <NavbarItem>
-          <Link href="/tours">Tours</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/users">Community</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/about">About us</Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end" className="flex justify-end space-x-0">
-        <NavbarItem>
-          <Button as={Link} href="/login" color="primary" variant="bordered">
-            Login
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} href="/signup" color="primary" variant="solid">
-            Signup
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+    <>
+      {loginFormVisible && <LoginForm loginFormVisible={setLoginFormVisible} />}
+
+      <Navbar className="flex min-w-full z-40 shadow-sm">
+        <NavbarContent justify="start">
+          <NavbarItem>
+            <Link href="/tours">Tours</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/users">Community</Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/about">About us</Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end" className="flex justify-end space-x-0">
+          {!loginFormVisible && (
+            <NavbarItem>
+              <Button
+                onClick={() => setLoginFormVisible(true)}
+                href="/login"
+                color="primary"
+                variant="bordered"
+              >
+                Login
+              </Button>
+            </NavbarItem>
+          )}
+          <NavbarItem>
+            <Button as={Link} href="/signup" color="primary" variant="solid">
+              Signup
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
   );
 }
