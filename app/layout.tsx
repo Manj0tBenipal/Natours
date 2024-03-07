473d0c626e6bd54185a90585c17d867d06d336b7
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-07 08:21:30 -0500

    moved providers to a separate folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import Providers from "@/components/Providers";
+import Providers from "@/components/providers/Providers";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Image from "next/image";
