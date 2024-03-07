2f56ddad848ab3472703b66db7446fae7224c8c6
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-07 11:47:57 -0500

    Implemented ServerActionRes interface to follow a single response pattern when dealing with server actions

--- utils/serverActions.ts
+++ utils/serverActions.ts
@@ -6,7 +6,7 @@ import { cookies } from "next/headers";
  * @param credentials email and password of user
  * @returns data
  */
-export async function login(credentials: string): Promise<string> {
+export async function login(credentials: string): Promise<ServerActionRes> {
   try {
     const res = await fetch(`${process.env.API_URL}/users/login`, {
       method: "POST",
@@ -14,31 +14,40 @@ export async function login(credentials: string): Promise<string> {
       headers: { "Content-type": "application/JSON" },
     });
     const statusCode = res.status;
-    if (statusCode !== 200) {
-      return JSON.stringify({ error: "Invalid Credentials" });
-    }
+
+    if (statusCode === 401) throw new Error("Invalid Credentials");
+
+    if (statusCode !== 200) throw new Error("Something went wrong!");
+
     const { data, token } = await res.json();
-    if (!data && !token)
-      return JSON.stringify({ error: "Something went wrong please try later" });
+
+    if (!data && !token) throw new Error("Something went wrong!");
+
     cookies().set({
       name: "refreshToken",
       value: token,
       httpOnly: true,
-      secure: true,
+      secure: process.env.APP_ENV === "production",
     });
-    return JSON.stringify(data);
+
+    return { status: "success", error: null, data };
   } catch (err) {
-    return JSON.stringify({ error: "Something went wrong please try later" });
+    return { status: "fail", error: "somehting went wrong", data: null };
   }
 }
 
-export async function authUsingCookie(): Promise<string> {
+/**
+ *This function
+ * @returns {Promse<ServerActionRes>}
+ */
+export async function authUsingCookie(): Promise<ServerActionRes> {
   try {
     const res = await fetch(`${process.env.API_URL}/users/auth-using-cookie`);
     if (res.status !== 200) throw new Error("User is not logged in");
     const { user } = (await res.json()).data;
-    return JSON.stringify(user);
+    if (!user) throw new Error("User not logged in");
+    return { status: "success", data: { user }, error: null };
   } catch (err: any) {
-    return JSON.stringify({ status: "fail", data: null, error: err.message });
+    return { status: "fail", data: null, error: err.message };
   }
 }
