42227490fa64e544fe4c27e031f83bb0242f8853
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-09 13:21:25 -0500

    added interface for detailed tour

--- utils/types.ts
+++ utils/types.ts
@@ -11,9 +11,39 @@ interface User {
 interface TourShort {
   _id: string;
   name: string;
-  ratingsAverage: string;
+  ratingsAverage: number;
   summary: string;
   imageCover: string;
   price: number;
   difficulty: string;
+}
+
+interface TourDetailed {
+  _id: string;
+  name: string;
+  startLocation: {
+    type: string;
+    coordinates: number[];
+    address: string;
+    description: string;
+  };
+  price: number;
+  duration: 14;
+  difficulty: "easy" | "difficult" | "hard";
+  ratingsQuantity: number;
+  ratingsAverage: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  startDates: Date[];
+  locations: Location[];
+  guides: User[];
+}
+interface Location {
+  type: string;
+  coordinates: number[];
+  description: string;
+  day: number;
+  _id: string;
 }
