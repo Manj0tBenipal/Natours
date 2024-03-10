b0bb020c1bc9b140e9b17c7a651d895ef2e0932f
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-10 01:37:53 -0500

    added skeleton loading

--- components/ReviewsSection.tsx
+++ components/ReviewsSection.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useEffect, useState } from "react";
-import { Pagination, Button } from "@nextui-org/react";
+import { Pagination, Button, Skeleton } from "@nextui-org/react";
 import ReviewCard from "./ui/ReviewCard";
+import SkeletonComponent from "./ui/Skeleton";
 
 export default function ReviewsSection({ tourId }: { tourId: string }) {
   const [fetching, setFetching] = useState<boolean>(true);
   const [totalPages, setTotalPages] = useState(0);
   const [reviews, setReviews] = useState<Review[]>([] as Review[]);
   const [currentPage, setCurrentPage] = useState(1);
+  const skeletons: JSX.Element[] = [];
+  for (let i = 0; i < 2; i++) {
+    skeletons.push(<SkeletonComponent />);
+  }
   useEffect(() => {
     async function fetchReviews() {
       try {
@@ -19,7 +24,6 @@ export default function ReviewsSection({ tourId }: { tourId: string }) {
           throw new Error(reviewsRes.err);
         const reviews: Review[] = reviewsRes.data;
         if (reviews?.length > 0) {
-          console.log(reviewsRes);
           setTotalPages(reviewsRes.totalPages);
           setReviews(reviews);
           setFetching(false);
@@ -31,51 +35,61 @@ export default function ReviewsSection({ tourId }: { tourId: string }) {
     }
     fetchReviews();
   }, [currentPage]);
-  return fetching ? (
-    <div>loading</div>
-  ) : (
+  return (
     <div className="flex flex-col gap-y-4">
       <h1 className="text-3xl text-gradient font-bold">Reviews</h1>
-      <div className="flex flex-col gap-y-2">
-        {reviews?.length > 0
-          ? reviews.map((review) => (
-              <ReviewCard key={review._id} review={review} />
-            ))
-          : "No reviews yet"}
-      </div>
-      <div className="flex flex-col gap-5">
-        <p className="text-small text-default-500">
-          Selected Page: {currentPage}
-        </p>
-        <Pagination
-          total={totalPages}
-          color="primary"
-          page={currentPage}
-          onChange={setCurrentPage}
-        />
-        <div className="flex gap-2">
-          <Button
-            size="sm"
-            variant="flat"
-            color="primary"
-            onPress={() =>
-              setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev))
-            }
-          >
-            Previous
-          </Button>
-          <Button
-            size="sm"
-            variant="flat"
-            color="primary"
-            onPress={() =>
-              setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev))
-            }
-          >
-            Next
-          </Button>
-        </div>
-      </div>
+      {fetching ? (
+        skeletons.map((s) => skeletons)
+      ) : (
+        <>
+          <div className="flex flex-col gap-y-2">
+            {reviews?.length > 0
+              ? reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))
+              : "No reviews yet"}
+          </div>
+          <div className="flex flex-col gap-5">
+            <p className="text-small text-default-500">
+              Selected Page: {currentPage}
+            </p>
+            <Pagination
+              total={totalPages}
+              color="primary"
+              page={currentPage}
+              onChange={setCurrentPage}
+            />
+            <div className="flex gap-2">
+              <Button
+                size="sm"
+                variant="flat"
+                color="primary"
+                onPress={() =>
+                  setCurrentPage((prev) => {
+                    setFetching(true);
+                    return prev > 1 ? prev - 1 : prev;
+                  })
+                }
+              >
+                Previous
+              </Button>
+              <Button
+                size="sm"
+                variant="flat"
+                color="primary"
+                onPress={() =>
+                  setCurrentPage((prev) => {
+                    setFetching(true);
+                    return prev < totalPages ? prev + 1 : prev;
+                  })
+                }
+              >
+                Next
+              </Button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
