f43e1939cf45136d15b0523dc8fe37b378ac39b0
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-10 18:18:28 -0400

    triggered reload of component when a new review is added

--- components/ReviewsSection.tsx
+++ components/ReviewsSection.tsx
@@ -10,36 +10,41 @@ export default function ReviewsSection({ tourId }: { tourId: string }) {
   const [totalPages, setTotalPages] = useState(0);
   const [reviews, setReviews] = useState<Review[]>([] as Review[]);
   const [currentPage, setCurrentPage] = useState(1);
+
+  //This state is used to trigger a useEffect to refetch the reviews when
+  //a user adds a new review
+  const [reloadReviews, setRelaodReviews] = useState<boolean>(false);
   const skeletons: JSX.Element[] = [];
   for (let i = 0; i < 2; i++) {
     skeletons.push(<SkeletonComponent key={i} />);
   }
-  useEffect(() => {
-    async function fetchReviews() {
-      try {
-        const reviewsPromise = await fetch(
-          `/api/reviews/${tourId}${currentPage > 1 ? "?page=" + currentPage : ""}`
-        );
-        const reviewsRes = await reviewsPromise.json();
-        if (reviewsRes.status === "fail" || reviewsRes.err)
-          throw new Error(reviewsRes.err);
-        const reviews: Review[] = reviewsRes.data;
-        if (reviews?.length > 0) {
-          setTotalPages(reviewsRes.totalPages);
-          setReviews(reviews);
-          setFetching(false);
-        }
-      } catch (err) {
-        console.log(err);
+  async function fetchReviews() {
+    try {
+      setFetching(true);
+      const reviewsPromise = await fetch(
+        `/api/reviews/${tourId}${currentPage > 1 ? "?page=" + currentPage : ""}`
+      );
+      const reviewsRes = await reviewsPromise.json();
+      if (reviewsRes.status === "fail" || reviewsRes.err)
+        throw new Error(reviewsRes.err);
+      const reviews: Review[] = reviewsRes.data;
+      if (reviews?.length > 0) {
+        setTotalPages(reviewsRes.totalPages);
+        setReviews(reviews);
         setFetching(false);
       }
+    } catch (err) {
+      console.log(err);
+      setFetching(false);
     }
+  }
+  useEffect(() => {
     fetchReviews();
-  }, [currentPage]);
+  }, [currentPage, reloadReviews]);
   return (
     <div className="flex flex-col gap-y-4  bg-slate-200 rounded-2xl p-4">
       <h1 className="text-3xl text-gradient font-bold">Reviews</h1>
-      <AddReview />
+      <AddReview tourId={tourId} reloadReviews={setRelaodReviews} />
       <Divider orientation="horizontal" />
       {fetching ? (
         skeletons.map((s) => skeletons)
@@ -68,10 +73,7 @@ export default function ReviewsSection({ tourId }: { tourId: string }) {
                 variant="flat"
                 color="primary"
                 onPress={() =>
-                  setCurrentPage((prev) => {
-                    setFetching(true);
-                    return prev > 1 ? prev - 1 : prev;
-                  })
+                  setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev))
                 }
               >
                 Previous
@@ -82,13 +84,9 @@ export default function ReviewsSection({ tourId }: { tourId: string }) {
                 variant="flat"
                 color="primary"
                 onPress={() =>
-                  setCurrentPage((prev) => {
-                    if (prev < totalPages) {
-                      setFetching(true);
-                      return prev < totalPages ? prev + 1 : prev;
-                    }
-                    return prev;
-                  })
+                  setCurrentPage((prev) =>
+                    prev < totalPages ? prev + 1 : prev
+                  )
                 }
               >
                 Next
