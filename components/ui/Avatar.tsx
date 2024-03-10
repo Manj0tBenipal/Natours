6c819f04f15bd755160b58138492f6d93efb2110
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-10 01:23:32 -0500

    created simple userAvatar

---
+++ components/ui/Avatar.tsx
@@ -0,0 +1,14 @@
+import { User } from "@nextui-org/react";
+
+export default function Avatar({ user }: { user: User }) {
+  return (
+    <User
+      name={user.name}
+      description={user.email}
+      avatarProps={{
+        src: user.photo,
+        className: "w-10 h-10",
+      }}
+    ></User>
+  );
+}
