86a65f798a36250b4de2f4d3869d0aac14107154
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-11 00:03:59 -0400

    delete button added when a user s author if the review

--- components/ui/ReviewCard.tsx
+++ components/ui/ReviewCard.tsx
@@ -1,14 +1,18 @@
 "use client";
+import { deleteReview } from "@/utils/serverActions";
 import { Avatar, Card, CardBody, CardFooter, Divider } from "@nextui-org/react";
+import { Dispatch, SetStateAction } from "react";
 import { FaStar } from "react-icons/fa";
 import { FaTrashCan } from "react-icons/fa6";
 
 export default function ReviewCard({
   review,
   currentUser,
+  reloadReviews,
 }: {
   review: Review;
   currentUser: string;
+  reloadReviews: Dispatch<SetStateAction<boolean>>;
 }) {
   const stars: JSX.Element[] = [] as JSX.Element[];
   for (let i = 1; i <= 5; i++) {
@@ -20,12 +24,19 @@ export default function ReviewCard({
       )
     );
   }
+  const handleDeleteReview = async () => {
+    if (review._id) await deleteReview(review._id);
+    reloadReviews((prev) => !prev);
+  };
   return (
     <Card>
       <CardBody className="flex gap-y-2 justify-center items-start">
         {currentUser === review.user._id && (
           <div className="absolute right-4 top-4">
-            <FaTrashCan className="cursor-pointer z-50" />
+            <FaTrashCan
+              className="cursor-pointer z-50"
+              onClick={handleDeleteReview}
+            />
           </div>
         )}
         <div className="flex items-center gap-x-2">
