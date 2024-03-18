4226ad279fea90a5e78df4da08a997dae6d026f1
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-18 10:36:12 -0400

    added server action to retrieve lati and lng using a given address

--- utils/server_actions/maps.ts
+++ utils/server_actions/maps.ts
@@ -26,3 +26,34 @@ export async function getLocations(location: string): Promise<ServerActionRes> {
     return { data: null, status: "fail", error: err.message };
   }
 }
+
+/**
+ * This function retieves loongitude and latitude of an address
+ * @param address
+ * @returns {ServerActionRes} data containing latitude and longitude of a given address
+ */
+export async function getLatLngFromAddress(
+  address: string
+): Promise<ServerActionRes> {
+  try {
+    const encodedAddress = encodeURIComponent(address);
+    const promise = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${env("MAPS_API_KEY")}`
+    );
+    const res = await promise.json();
+    const { lat, lng } = res.results[0]?.geometry?.location;
+    if (!lng || !lat) throw new Error("Failed to get address data from Google");
+    if (res.status !== "OK" || res.results.length === 0)
+      throw new Error("Failed to get address data from Google");
+    return {
+      error: null,
+      data: {
+        lat,
+        lng,
+      },
+      status: "success",
+    };
+  } catch (err: any) {
+    return { data: null, status: "fail", error: err.message };
+  }
+}
